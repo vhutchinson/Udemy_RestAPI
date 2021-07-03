3d695f59f6e8be510d03582e26996ce25164481f
vhutchinson <virginiaehutchinson@gmail.com>
2021-07-03 11:48:34 -0600

    Initial commit: Initialize API project with Model and Controller

---
+++ CatsAPI/Controllers/MyCatsController.cs
@@ -0,0 +1,47 @@
+﻿using CatsAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyCatsController : ControllerBase
+    {
+        private static List<MyCat> myCats = new List<MyCat>
+        {
+            new MyCat {Name = "Penny", Type = "Torbie", Age = 7, Weight = 8.5},
+            new MyCat {Name = "Louise", Type = "Potato", Age = 2, Weight = 16.0}
+        };
+
+        [HttpGet]
+        public IEnumerable<MyCat> Get()
+        {
+            return myCats;
+        }
+
+        [HttpPost]
+        public void Post([FromBody]MyCat newCat)
+        {
+            myCats.Add(newCat);
+        }
+
+        [HttpPut("{name}")]
+        public void Put(string name, [FromBody]MyCat cat)
+        {
+            int namedCatIndex = myCats.IndexOf(myCats.Where(i => i.Name == name).FirstOrDefault());
+            myCats[namedCatIndex] = cat;
+        }
+
+        [HttpDelete("{name}")]
+        public void Delete(string name)
+        {
+            int namedCatIndex = myCats.IndexOf(myCats.Where(i => i.Name == name).FirstOrDefault());
+            myCats.RemoveAt(namedCatIndex);
+        }
+    }
+}
---
+++ CatsAPI/Models/MyCat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatsAPI.Models
+{
+    public class MyCat
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Age { get; set; }
+        public double Weight { get; set; }
+    }
+}
