6c906e5014199556cef9294bd098f8716f825e0b
vhutchinson <virginiaehutchinson@gmail.com>
2021-07-05 15:15:14 -0600

    Made MyCat properties required (Name, Type, Age, Weight).

--- CatsAPI/Models/MyCat.cs
+++ CatsAPI/Models/MyCat.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,12 +11,22 @@ namespace CatsAPI.Models
     public class MyCat
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "This cat needs a non-empty name! Not that it will ever answer to that name.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter this cat's type.")]
         public string Type { get; set; }
+
+        [Required(ErrorMessage = "Please enter this cat's age.")]
         public int Age { get; set; }
+
+        [Required(ErrorMessage = "Please enter this cat's weight.")]
         public double Weight { get; set; }
+
         [NotMapped]
         public IFormFile Image { get; set; }
+
         public string ImageUrl { get; set; }
     }
 }
